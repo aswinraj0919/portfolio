1d636cbf04747057f2f5651fe911b4122096bd57
aswinraj0919 <aswinraj.stackup@gmail.com>
2024-08-30 01:00:48 +0530

    first commit

--- script.js
+++ script.js
@@ -21,42 +21,61 @@ document.querySelectorAll('.hover-target').forEach((item) => {
         cursor3.classList.remove('hover');
     });
 });
+const aboutText = document.querySelector('.about-text');
+const contactText = document.querySelector('.contact-text');
+const SkillsText = document.querySelector('.Skills');
+const ProjectsText = document.querySelector('.Projects');
+const natureText = document.querySelector('.nature');
 
-$(".about-text").on('click', function () {
-	$("body").addClass("about-on");
-});
-$(".about-close").on('click', function () {
-	$("body").removeClass("about-on");
-});
+const aboutSection = document.querySelector('.about-section');
+const contactSection = document.querySelector('.contact-section');
+const SkillsSection = document.querySelector('.Skills-section');
+const ProjectsSection = document.querySelector('.Projects-section');
+const natureSection = document.querySelector('.nature-section');
 
+const aboutClose = document.querySelector('.about-close');
+const contactClose = document.querySelector('.contact-close');
+const SkillsClose = document.querySelector('.Skills-close');
+const ProjectsClose = document.querySelector('.Projects-close');
+const natureClose = document.querySelector('.nature-close')
+const body=document.querySelector(".body")
 
-$(".contact-text").on('click', function () {
-	$("body").addClass("contact-on");
-});
-$(".contact-close").on('click', function () {
-	$("body").removeClass("contact-on");
-});
-
-
-$(".Skills").on('click', function () {
-	$("body").addClass("Skills-on");
-});
-$(".Skills-close").on('click', function () {
-	$("body").removeClass("Skills-on");
-});
-
-
-$(".Projects").on('click', function () {
-	$("body").addClass("Projects-on");
-});
-$(".Projects-close").on('click', function () {
-	$("body").removeClass("Projects-on");
+aboutText.addEventListener('click', () => {
+    body.classList.add("about-on");
 });
 
-
-$(".nature").on('click', function () {
-	$("body").addClass("nature-on");
+contactText.addEventListener('click', () => {
+    body.classList.add("contact-on");
 });
-$(".nature-close").on('click', function () {
-	$("body").removeClass("nature-on");
+
+SkillsText.addEventListener('click', () => {
+    body.classList.add("Skills-on");
+});
+
+ProjectsText.addEventListener('click', () => {
+    body.classList.add("Projects-on");
+});
+
+natureText.addEventListener('click', () => {
+    body.classList.add("nature-on");
+});
+
+aboutClose.addEventListener('click', () => {
+    body.classList.remove("about-on");
+});
+
+contactClose.addEventListener('click', () => {
+    body.classList.remove("contact-on");
+});
+
+SkillsClose.addEventListener('click', () => {
+    body.classList.remove("Skills-on");
+});
+
+ProjectsClose.addEventListener('click', () => {
+    body.classList.remove("Projects-on");
+});
+
+natureClose.addEventListener('click', () => {
+    body.classList.remove("nature-on");
 });
